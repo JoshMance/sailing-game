b59799aaeed30abda143168e68c35bcab032268d
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-12-31 11:22:46 +0800

    Adding wake effect (simple)

--- shipMovement.js
+++ shipMovement.js
@@ -2,13 +2,18 @@
 let canvas;
 let context;
 
+// Note: split up the movement logic and the drawing
+
 var shipAngle = 0; 
+var shipSpeed = 0;
 var windAngle = Math.PI; 
-var shipX = 250; 
+var shipX = 500; 
 var shipY = 250;
 
+var shipPath = [[shipX, shipY]];
+
 function getShipSpeed(shipAngle, windAngle) {
-    let theta = (Math.PI - windAngle + shipAngle) % (2*Math.PI);
+    let theta = ((3*Math.PI/2) - windAngle + shipAngle) % (2*Math.PI);
     let speed = -1*(Math.sin(2-Math.sin(theta)) + Math.cos(theta)*Math.cos(theta));
     return speed;
 }
@@ -33,7 +38,7 @@ function init() {
 }
 
 function handleKeydown(event) {
-    const rotationSpeed = 0.07; // Rotation speed in radians
+    const rotationSpeed = 0.1; // Rotation speed in radians
 
     switch (event.key) {
         case 'a':
@@ -50,6 +55,7 @@ function handleKeydown(event) {
 }
 
 function gameLoop(timeStamp) {
+
     draw();
 
     // Keep requesting new frames
@@ -57,47 +63,86 @@ function gameLoop(timeStamp) {
 }
 
 function draw() {
-    let width = 30;
-    let height = 100;
-
     context.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+    drawWake();
+    drawShip();
+    drawUI();
+}
 
-    context.fillStyle = '#ff8080';
+function drawShip() {
+    let width = 20;
+    let height = 60;
 
-    var offsetX = shipX - canvas.width / 2;
-    var offsetY = shipY - canvas.height / 2;
+    context.fillStyle = '#7c4426';
 
     // Save the current context state
     context.save();
 
-    context.translate(offsetX, offsetY); // Translate the canvas
+    // Update ship position
+    shipSpeed = getShipSpeed(shipAngle, windAngle);
+    shipX += Math.sin(shipAngle) * shipSpeed;
+    shipY -= Math.cos(shipAngle) * shipSpeed;
 
-    let shipSpeed = getShipSpeed(shipAngle, windAngle);
-    shipX += 0.3*Math.sin(shipAngle)*shipSpeed;
-    shipY -= 0.3*Math.cos(shipAngle)*shipSpeed; 
+    shipPath.push([shipX, shipY])
 
-    // Move the origin to the center of the rectangle
-    context.translate(shipX + width / 2, shipY + height / 2);
-    
-    // Rotate the context based on the ship angle
+    // Translate and rotate the canvas
+    context.translate(shipX, shipY);
     context.rotate(shipAngle);
+
+    // Drawing the base rectangle for the ship
+
+    // Drawing the border of the ship
+    context.beginPath(); // Starting a new path to avoid persisting old paths
+
+    context.moveTo(width/2, height/2);
+    context.quadraticCurveTo(-width/5, 0, width / 2, -height / 2);
+
+    context.moveTo(-width / 2, (height / 2)-1); 
+    context.bezierCurveTo(0.4, height - 0.35,
+                          0.6, height - 0.5,
+                          width/2, (height / 2)-1);
+
+
+    context.moveTo(-width / 2, -height / 2);
+    context.quadraticCurveTo(0, -height*(3/5), width / 2, -height / 2);
+
     
-    // Draw the rectangle, with the new transformed coordinates
-    context.fillRect(-width / 2, -height / 2, width, height);
-    
+
+    context.fill();
+    context.closePath();
+
     // Restore the context to its previous state
     context.restore();
 
+}
 
-    context.font = "bold 20px serif"
+function drawWake () {
+    context.save();
+
+    for (let i = 0; i < shipPath.length; i++) {
+        let x = shipPath[i][0];
+        let y = shipPath[i][1];
+        context.arc(x, y, 10, 0, Math.PI);
+        context.strokeStyle = "#ffffff13";
+      }
+      context.stroke();
+    context.restore();
+}
+
+
+function drawUI() {
+
+    context.save();
+    
+    context.font = "20px sans"
     context.fillStyle = "#000000"
 
     let degrees = Math.round(shipAngle*(180/Math.PI),2) % 360;
     
     let speed = Math.round((shipSpeed + Number.EPSILON) * 100) / 100
 
-    context.fillText(`Speed = ${speed}`, 30, 50);
-    context.fillText(`Angle = ${degrees}°`, 30, 80);
+    context.fillText(`Speed: ${speed}`, 30, 50);
+    context.fillText(`Angle: ${degrees}°`, 30, 80);
 
-
+    context.restore();
 }
